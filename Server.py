0de5573a64a9bd5dc0d740bafab62a52215a4d4b
matt-culbert <culbertmatt@gmail.com>
2022-02-02 03:30:14 +0000

    Update Server.py

--- Server.py
+++ Server.py
@@ -3,19 +3,39 @@
 import http.server as SimpleHTTPServer
 import socketserver as SocketServer
 import logging
+import ssl
 
 PORT = 8000
+test = {}
 
 class GetHandler(
         SimpleHTTPServer.SimpleHTTPRequestHandler
         ):
 
     def do_GET(self):
-        logging.error(self.headers)
-        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+        temp = self.headers # Grab the headers
+        temp = str(temp) # Make them a string
+        temp = temp.splitlines() # Split it into a list
+        temp = str(temp[-2]) # Get the last item which is the ID and make it a string(stringception)
+        if 'ID' in temp: # Here we can do actions based on the ID returned
+            print(temp)
+        self.send_response(200)
+        self.send_header("Test Dictionary Value", test) # Here we can make a custom response, perhaps pull a command from a queue
+        self.end_headers()
 
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # Gets the size of data
+        post_data = self.rfile.read(content_length) # Gets the data itself
+
+        print(post_data)
+
+        test['ID'] = post_data # Add the ID given to us by the beacon to the dict - this should be replaced with a persistent mechanism, something like rabbidmq
 
 Handler = GetHandler
 httpd = SocketServer.TCPServer(("127.0.0.1", PORT), Handler)
+
+#httpd.socket = ssl.wrap_socket (httpd.socket,
+#        keyfile="/etc/nginx/certs/server.key",
+#        certfile='/etc/nginx/certs/server.crt', server_side=True)
 
 httpd.serve_forever()
